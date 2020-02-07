5d7d1cf1cb420ec279e4cf057df8e8985af06b8f
yosuke.kaneko <yosuke-kaneko@x-power.co.jp>
2020-02-07 16:30:58 +0900

    add Report class

---
+++ src/models/validators/ReportValidator.java
@@ -0,0 +1,43 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Report;
+
+public class ReportValidator {
+ // バリデーション(入力値チェック)を実行する
+    public static List<String> validate(Report r) {
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = _validateTitle(r.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = _validateContent(r.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // タイトルの必須入力チェック
+    private static String _validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+        }
+
+        return "";
+    }
+
+    // 内容の必須入力チェック
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "内容を入力してください。";
+        }
+
+        return "";
+    }
+}
